ee8a08974d1e25ed28c6c5e36b3c3f07b22af5dd
AlanCastellanos <alancastellanos117@gmail.com>
2018-02-25 23:46:17 -0600

    Initial commit

---
+++ src/practicaWGrafica/CalculadorImpuestos.java
@@ -0,0 +1,13 @@
+package practicaWGrafica;
+
+public class CalculadorImpuestos {
+
+    public double calcularPago(String unaMarca, String unaLinea, String unModelo, boolean descProntoPago, boolean descServicioPublico, boolean descTrasladoCuenta) {
+        return 0;
+    }
+
+    public double buscarAvaluoVehiculo(String unaMarca, String unaLinea, String unModelo) {
+        return 0;
+    }
+
+}
